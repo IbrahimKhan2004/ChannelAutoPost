144f1d5708e6c1a7bf93f00191e828780483181e
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2022-12-12 14:04:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from telethon import TelegramClient, events, Button
 from decouple import config
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+    format="[%(levelname) 10s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 
 # start the bot
